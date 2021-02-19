48a8afb853e5269f28e0d522dc07e4c141ee349e
ogawaluan <ogawa.luan@gmail.com>
2021-02-19 19:23:00 -0300

    creating sessions/user routes and auth middleware

--- backend/src/routes/sessions.routes.ts
+++ backend/src/routes/sessions.routes.ts
@@ -1,9 +1,27 @@
 import { Router } from 'express';
 
+import CreateSessionService from '../services/CreateSessionService';
+
 const sessionsRouter = Router();
 
-sessionsRouter.post('/', (request, response) => {
-  return response.json({ message: "hello world" });
+sessionsRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticateUser = new CreateSessionService();
+
+    const { user, token } = await authenticateUser.execute({
+      email,
+      password
+    });
+
+    //@ts-ignore
+    delete user.password;
+
+    return response.json({ user, token });
+  } catch(err) {
+    return response.status(400).json({ message: err.message });
+  }
 });
 
 export default sessionsRouter;
